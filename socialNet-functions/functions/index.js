1757b6fbfd719dd0a5919a1a9a634316c93b5413
Aditya Pandey <addherbs@gmail.com>
2020-06-04 22:24:08 -0500

    Added firestore function which creates notification on scream likes

--- socialNet-functions/functions/index.js
+++ socialNet-functions/functions/index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const functions = require('firebase-functions');
-
+const { db } = require('./util/admin');
 
 const { getAllScreams, 
     postOneScream, 
@@ -42,3 +42,30 @@ app.get('/user', FBAuth, getAuthenticatedUser);
 
 
 exports.api = functions.https.onRequest(app);
+
+
+exports.createNotificationOnLike = functions.firestore.document('likes/{id}')
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`).get()
+            .then(doc => {
+                if(doc.exists){
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'like',
+                        read: false,
+                        screamId: snapshot.data().screamId      // doc.id => screamId
+                    });
+                }
+            })
+            .then(() => {
+                return;
+            })
+            .catch(err => {
+                console.error(err);
+                return;
+            });
+    });
+
+    
