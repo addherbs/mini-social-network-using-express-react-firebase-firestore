6dafc283be8b08ad804d74582e64657e99463a2f
Aditya Pandey <addherbs@gmail.com>
2020-06-14 12:05:49 -0500

    Added some more structural related design and code for front end

--- socialnet-client/src/App.js
+++ socialnet-client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
 import './App.css';
- 
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup'; 
+
 function App() {
   return (
     <div className="App">
-      <h1>Our App </h1>
+      <Router>
+        <Switch>
+          <Route path='/' component={home}/>
+          <Route path='/login' component={login}/>
+          <Route path='/signup' component={signup}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
